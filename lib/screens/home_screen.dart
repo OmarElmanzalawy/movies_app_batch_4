ae1fb9436ab9512f0ea32c0122bdd475d8b9f5d6
Omar Elmanzalawy <omarmanz2002@gmail.com>
2026-02-01 18:30:02 +0200

    add view model

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/view_model/view_model.dart';
 import 'package:movies_app/widgets/category_capsule.dart';
 import 'package:movies_app/widgets/movie_card.dart';
 
@@ -16,13 +17,18 @@ class HomeScreen extends StatelessWidget {
           IconButton(onPressed: (){}, icon: Icon(Icons.bedtime)),
         ],
         ),
-      body: ListView(
+      body: vm.movies.isEmpty ? Center(
+              child: Text("No movies yet"),
+            ) : 
+        ListView.builder(
         padding: EdgeInsets.only(top: 30,right: 12,left: 12),
-        children: [
-         MovieCard(),
-         MovieCard(),
-        ],
+        itemCount: vm.movies.length,
+        itemBuilder: (context, index) {
+          return MovieCard();
+        },
       )
     );
   }
 }
+
+//eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNjVhMjZlY2M0MGFlNDc5OWE0NmE5ZDNiYWYwMWE1NCIsIm5iZiI6MTc2OTk0ODU1Ni4zMjgsInN1YiI6IjY5N2Y0NThjMjBjOWExZDU5MTcwZTkwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FHO94nSCxrev5SGSM9_w4DcCJ4WpEx4urSHWMszHRt8
---
+++ lib/view_model/view_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/models/movie_model.dart';
+
+final vm = ViewModel();
+
+class ViewModel {
+
+  List<MovieModel> movies = [];
+
+}
